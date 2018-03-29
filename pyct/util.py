c0dd33a8291b3e6c2b38c34d365613ff44e2dc1b
C. E. Ball <ceball@gmail.com>
2018-03-29 16:26:53 +0100

    Fix handling of missing tox sections.

--- pyct/util.py
+++ pyct/util.py
@@ -1,18 +1,13 @@
-import warnings
 import configparser
 
 toxconf = configparser.ConfigParser()
 toxconf.read('tox.ini')
 
-if len(toxconf.sections()) == 0:
-    warnings.warn("Could not read tox.ini; will use defaults (some commands may be unavailable and/or incorrect for your project).")
-    # TODO: actually, things just won't work. Should we have a default
-    # tox.ini in pyct? Or require projects to have a tox.ini (which
-    # would force them to declare what tests there are, and how to run
-    # them).
-
 def get_tox_cmds(env):
-    return toxconf[env]['commands'].splitlines()
+    if env in toxconf:
+        return toxconf[env]['commands'].splitlines()
+    else:
+        return ["""python -c "print('Could not read """ + env + """ from tox.ini');import sys;sys.exit(1)" """]
 
 
 _options_param = {
