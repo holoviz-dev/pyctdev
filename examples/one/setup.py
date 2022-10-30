986d95d5c6dfa337e411ad910bc69f25ed2a4e26
maximlt <mliquet@anaconda.com>
2022-10-30 20:39:40 +0100

    apply black once and for all

--- examples/one/setup.py
+++ examples/one/setup.py
@@ -2,26 +2,26 @@ from setuptools import setup, find_packages
 
 
 setup_args = dict(
-    name='one',
-    version='0.0.1',
+    name="one",
+    version="0.0.1",
     python_requires=">=2.7",
-    packages = find_packages(),
-    install_requires = [
+    packages=find_packages(),
+    install_requires=[
         # just example
-        'hypothesis[numpy] <3.57.0'
+        "hypothesis[numpy] <3.57.0"
         # TODO: need a [] without a version to test cb dropping as selector
     ],
-    extras_require = {
-        'tests': ['flake8'],
-        'examples': [
-            'holoviews[recommended] <=1.10.0', # pulls in other deps and pinned (both deliberate, for testing)
-        ]
+    extras_require={
+        "tests": ["flake8"],
+        "examples": [
+            "holoviews[recommended] <=1.10.0",  # pulls in other deps and pinned (both deliberate, for testing)
+        ],
     },
-    url = "http://",
-    license = "BSD",
-    description = "Example"
+    url="http://",
+    license="BSD",
+    description="Example",
 )
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     setup(**setup_args)
