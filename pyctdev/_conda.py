af9ec153bc289a820fe30b7e5de59d4817404912
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2023-01-03 15:50:53 +0100

    Fix mapping recipe (#108)

--- pyctdev/_conda.py
+++ pyctdev/_conda.py
@@ -710,7 +710,7 @@ def task_package_build():
 
     force = {"name": "force", "long": "force", "type": bool, "default": False}
 
-    def create_base_recipe(package_name, force):
+    def create_base_recipe(package_name, force, pkg_tests):
 
         # TODO: need to replace this with checking for existing recipe and using that.
         # and fall back to package name in normal setup.cfg
@@ -755,6 +755,9 @@ def task_package_build():
                 "[" + ",".join('"%s"' % _join_the_club(dep) for dep in buildreqs) + "]"
             )
             f.write("{%% set builddeps = %s %%}\n" % buildeps)
+            f.write("{%% set need_tests_deps = %s %%}\n" % pkg_tests)
+            namespace_map = read_conda_namespace_map("setup.cfg")
+            f.write("{%% set namespace_map = %s %%}\n" % namespace_map)
             f.write(r)
 
     def create_recipe_clobber(recipe, pin_deps_as_env, no_pin_deps, package_name):
